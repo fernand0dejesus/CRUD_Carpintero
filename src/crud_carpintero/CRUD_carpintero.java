f3a550b20888aeaaeb4e6bd1bfb7523f7b125bd5
Manuel <20220416@ricaldone.edu.sv>
2024-09-24 12:10:21 -0600

    Primer commit

---
+++ src/crud_carpintero/CRUD_carpintero.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crud_carpintero;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class CRUD_carpintero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
